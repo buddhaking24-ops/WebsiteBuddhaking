f38aca57a2a30d46d4628ca4a6b585ba7421c807
buddhaking24-ops <buddhaking24@gmail.com>
2026-04-02 11:15:31 +0700

    update seo

--- src/main/java/com/buddhaking/controller/TrangChuController.java
+++ src/main/java/com/buddhaking/controller/TrangChuController.java
@@ -1,5 +1,6 @@
 package com.buddhaking.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,27 +12,41 @@ public class TrangChuController {
 	@GetMapping("")
 	public String Home(Model model) {
 		model.addAttribute("title","Hương nhang thảo mộc Buddha King – Nhang sạch tinh khiết");
+		 model.addAttribute("canonical", "https://buddhaking.vn/");
+		model.addAttribute("description","Hương nhang thảo mộc Buddha King – nhang sạch tinh khiết từ nguyên liệu tự nhiên, an toàn cho sức khỏe và môi trường.");
 		model.addAttribute("content","home");
 		return "index";
 	}
 	
-	@GetMapping("/contact")
+	@GetMapping("/lien-he")
 	public String contact(Model model) {
-		model.addAttribute("title","Thông tin liên hệ Buddha King – Hỗ trợ khách hàng");
+		model.addAttribute("title","Liên hệ Buddha King | Thông tin, hotline, hỗ trợ khách hàng");
+		model.addAttribute("canonical", "https://buddhaking.vn/lien-he");
+		model.addAttribute("description",
+			    "Liên hệ Buddha King để được tư vấn sản phẩm, hỗ trợ nhanh chóng qua hotline, email");
 		model.addAttribute("content","contact");
 		return "index";
 	}
 	
-	@GetMapping("/CNSH")
+	@GetMapping("/blog/cong-he-sinh-hoc-nhang-buddhaking")
 	public String CNSH(Model model) {
 		model.addAttribute("title","Công nghệ sản xuất nhang sạch thảo mộc | Buddha King");
+		 model.addAttribute("canonical", "https://buddhaking.vn/blog/cong-he-sinh-hoc-nhang-buddhaking");
+		model.addAttribute("description","Công nghệ sản xuất nhang sạch thảo mộc Buddha King sử dụng quy trình “Thanh lọc sinh học” (Bioremediation) độc quyền, giúp loại bỏ tạp chất, giữ trọn tinh dầu gỗ quý và thảo mộc tự nhiên");
 		model.addAttribute("content","CNSH");
 		return "index";
 	}
 	
-	@GetMapping("/about")
+	@GetMapping("/blog/gioi-thieu")
+	public ResponseEntity<Void> redirect0(Model model) {
+		return ResponseEntity.status(301).header("Location", "/gioi-thieu").build();
+	}
+	
+	@GetMapping("/gioi-thieu")
 	public String about(Model model) {
 		model.addAttribute("title","Giới thiệu thương hiệu nhang thảo mộc | Buddha King");
+		model.addAttribute("canonical", "https://buddhaking.vn/gioi-thieu");
+		model.addAttribute("description","Tìm hiểu về thương hiệu Buddha King – chuyên cung cấp nhang thảo mộc sạch, an toàn, nguồn gốc tự nhiên, mang đến không gian thanh tịnh và an lành.");
 		model.addAttribute("content","about");
 		return "index";
 	}
@@ -39,6 +54,8 @@ public class TrangChuController {
 	@GetMapping("/cart")
 	public String cart(Model model) {
 		model.addAttribute("title","Đặt mua nhang thảo mộc chính hãng | Buddha King");
+		model.addAttribute("description","Đặt mua nhang thảo mộc Buddha King chính hãng nhanh chóng, tiện lợi. Sản phẩm tự nhiên, không hóa chất, giao hàng toàn quốc.");
+		model.addAttribute("robots","noindex,nofollow");
 		model.addAttribute("content","cart");
 		return "index";
 	}
